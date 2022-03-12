5a0c82cd4135d8902eea20b35f5c8d83a522b7f6
SEBASTIAN-ALES1 <>
2022-03-12 20:49:20 +0000

    new try

--- exercises.c
+++ exercises.c
@@ -110,7 +110,7 @@ la cual retorna el valor en la posición i del vector v.
 */
 int obtenerValor(Vector * v, int i) {
     Vector *vh = (Vector* )malloc (sizeof(Vector));
-   return vh;
+   return *vh;
 }
 
 /*
