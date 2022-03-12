aac734db995228fedd7ae1f8e57728e25943d6ad
SEBASTIAN-ALES1 <>
2022-03-12 21:00:43 +0000

    new try

--- exercises.c
+++ exercises.c
@@ -113,7 +113,7 @@ actualiza el vector `c` con el resultado de la suma.
 */
 void sumaV(Vector * a, Vector * b, Vector * c) {
   int i=0;
-  a->datos[i]=(b->datos[i])+(c->datos[i]);
+  a->datos[i]=((b->datos[i])+(c->datos[i]));
   printf("VALORES DE AHORA %d \n",a->datos[i]);
   i++;
 }
