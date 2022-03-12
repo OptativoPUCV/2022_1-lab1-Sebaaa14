f98e35c5f3a2019922171c9c7776c61a31beed85
SEBASTIAN-ALES1 <>
2022-03-12 14:57:28 +0000

    new try

--- exercises.c
+++ exercises.c
@@ -96,8 +96,8 @@ Programe la función void asignarValor(Vector * v, int i, int valor),
 la cual asigna el valor a la posición i del vector v.
 */
 void asignarValor(Vector * v, int i, int valor) {
-      printf("PRINTEANDO DATOS %d\n",v->datos[i]);
-    v->datos[i]=valor;
+      printf("PRINTEANDO DATOS %d\n",v->datos);
+    v->datos=valor;
 
 }
 
