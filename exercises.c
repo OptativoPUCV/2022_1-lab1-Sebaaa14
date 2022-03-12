98bf84ee88c9532482e5ad2ac2e7ee1d51e45ed7
SEBASTIAN-ALES1 <>
2022-03-12 17:32:55 +0000

    new try

--- exercises.c
+++ exercises.c
@@ -96,7 +96,7 @@ Programe la función void asignarValor(Vector * v, int i, int valor),
 la cual asigna el valor a la posición i del vector v.
 */
 void asignarValor(Vector * v, int i, int valor) {
-   Vector *h[5];     
+   Vector h[5];     
   
 }
 
