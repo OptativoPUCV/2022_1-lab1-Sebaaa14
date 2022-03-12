be9797d592ad42f497e1952dcbb4e8e844a9a061
SEBASTIAN-ALES1 <>
2022-03-12 20:39:13 +0000

    new try

--- exercises.c
+++ exercises.c
@@ -95,7 +95,7 @@ la cual asigna el valor a la posición i del vector v.
 */
 void asignarValor(Vector * v, int i, int valor) { 
   printf("EL VALOR DE DATOS ES %d \n",v->datos[i]);
-  v->datos=valor;
+  v[i]->datos=valor;
 }
 
 /*
