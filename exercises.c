1bf69129b58a0d2b15e8d5ea2eaae9f365908e95
SEBASTIAN-ALES1 <>
2022-03-12 04:52:20 +0000

    new try

--- exercises.c
+++ exercises.c
@@ -63,10 +63,9 @@ Persona* crearPersona(char nombre[], char rut[], int edad) {
   Persona *p = (Persona*) malloc (sizeof(Persona));
   strcpy (p->nombre,"Ignacio");
   strcpy (p->rut,"15489XXX-2");
-  strcpy (p->edad,"38");
+  strcpy (p.edad,38);
   
-  
-   return p;
+  return p;
 }
 
 /*
